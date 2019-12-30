64ca19c684479ece9f2bc0068ab4f1f08f1ac518
Diwakar Choudhary <diwakar.choudhary@enparadigm.com>
2019-12-30 15:56:23 +0530

    UI changes

--- accept_order.js
+++ accept_order.js
@@ -1,14 +1,24 @@
 function validateCustomer()
 {
-alert($("#customer_name").val());
-$.post('http://iotahub.in/StudentServlet',   // url
-       { myData: 'This is my data.' }, // data to be submit
-       function(data, status, jqXHR) {// success callback
-                alert("hello");
-        });
-
-
+var customerName=$("#customer_name").val();
+var mobileNumber=$("#contact_number").val();
+var pincode=$("#address_pincode").val();
+var address=$("#customer_address").val();
+var email=$("#customer_email").val();
+var quantity=$("#product_quantity").val();
+if(customerName=='' || mobileNumber=='' || pincode=='' || address=='' || email=='' || quantity=='')
+{
+alert("All fields are mandatory");
+return;
+}
 }
 function placeOrder()
 {
+
+$.post('http://iotahub.in/StudentServlet',   // url
+       { myData: 'This is my data.' }, // data to be submit
+       function(data, status) {// success callback
+                alert("hello");
+});
+
 }
